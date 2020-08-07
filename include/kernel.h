fcd54da01370276fcacefad830e8cd862d05dcd5
Luka Warren <lukawarren@icloud.com>
2020-08-07 16:40:16 +0100

    GDT corrections

--- include/kernel.h
+++ include/kernel.h
@@ -2,9 +2,8 @@
 #ifndef KERNEL_H
 #define KERNEL_H
 
-extern "C" void kernel_main(void);
-
 #include <stdint.h>
-void jeff(uint8_t irq);
+
+extern "C" void kernel_main(void);
 
 #endif
--- src/kernel.cpp
+++ src/kernel.cpp
@@ -10,6 +10,9 @@
 #include "interrupts/keyboard.h"
 #include "cli.h"
 
+TSS tssEntry;
+uint64_t GDTTable[4];
+
 extern "C" void kernel_main(void) 
 {
     VGA_Clear();
@@ -30,16 +33,13 @@ extern "C" void kernel_main(void)
     VGA_printf<uint16_t, true>((uint16_t)COM1.m_Com);
 
     // Create TSS
-    constexpr TSS tssEntry = CreateTSSEntry(0x0);
+    tssEntry = CreateTSSEntry(0x0);
 
     // Construct GDT entries
-    const uint64_t GDTTable[4] =
-    {
-        CreateGDTEntry(0, 0, 0), // GDT entry at 0x0 cannot be used
-        CreateGDTEntry(0x00000000, 0xffffffff, GDT_CODE_PL0), // Code starting from 4MB, 4MB large - 0x8
-        CreateGDTEntry(0x00000000, 0xffffffff, GDT_DATA_PL0), // Data starting from 8MB, 4MB large - 0x10
-        CreateGDTEntry((uint32_t) &tssEntry, sizeof(tssEntry), TSS_PL0)  // TSS, 0x18
-    };
+    GDTTable[0] = CreateGDTEntry(0, 0, 0); // GDT entry at 0x0 cannot be used
+    GDTTable[1] = CreateGDTEntry(0x00000000, 0xffffffff, GDT_CODE_PL0); // Code - 0x8
+    GDTTable[2] = CreateGDTEntry(0x00000000, 0xffffffff, GDT_DATA_PL0); // Data - 0x10
+    GDTTable[3] = CreateGDTEntry((uint32_t) &tssEntry, sizeof(tssEntry), TSS_PL0);  // TSS, 0x18
 
     // Load GDT
     LoadGDT(GDTTable, sizeof(GDTTable));
