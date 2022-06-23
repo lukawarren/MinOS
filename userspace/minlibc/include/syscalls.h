125590ea27d52e7c2d4e2f3a74aa990b72240f97
Luka Warren <lukawarren1@gmail.com>
2022-06-23 18:46:10 +0100

    Added PDCLib

--- userspace/minlibc/include/syscalls.h
+++
@@ -1,9 +0,0 @@
-#ifndef SYSCALLS_H
-#define SYSCALLS_H
-
-#include <stdint.h>
-#include <stddef.h>
-
-int syscall_args_2(uint32_t id, size_t n1, size_t n2);
-
-#endif
--- userspace/minlibc/src/stdio.c
+++
@@ -1,9 +0,0 @@
-#include "syscalls.h"
-
-int printf(const char *format, ...)
-{
-    syscall_args_2(0, format, 128);
-
-    // TODO: return the number of values that are printed
-    return 0;
-}
