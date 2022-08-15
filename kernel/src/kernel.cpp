ffc1a88b1c8dec4f2dc865db92c63670b7d3c086
Luka Warren <lukawarren1@gmail.com>
2022-08-15 23:06:33 +0100

    Added multiboot support

--- kernel/src/kernel.cpp
+++ kernel/src/kernel.cpp
@@ -1,15 +1,22 @@
 #include "interrupts/interrupts.h"
 #include "memory/allocator.h"
+#include "multiboot.h"
 #include "memory/elf.h"
 #include "io/uart.h"
 #include "cpu/cpu.h"
 #include "klib.h"
 
-extern "C" { void kmain(void); }
+extern "C" { void kmain(multiboot_info_t* multiboot_info, uint32_t eax); }
 
-void kmain(void)
+void kmain(multiboot_info_t* multiboot_info, uint32_t eax)
 {
     uart::init();
+
+    // Verify we're multiboot
+    assert(eax == MULTIBOOT_BOOTLOADER_MAGIC);
+    uart::write_string("Detected ");
+    uart::write_number(multiboot_info->mods_count);
+    uart::write_string(" multiboot modules\n");
 
     // Setup interrupts
     interrupts::load();
