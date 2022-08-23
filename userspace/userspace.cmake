cf4537bf4a7c98711efbd339cb310d48e8ed8525
Luka Warren <lukawarren1@gmail.com>
2022-08-23 20:07:43 +0100

    CXX test

--- userspace/userspace.cmake
+++ userspace/userspace.cmake
@@ -2,4 +2,7 @@ add_link_options("-T${CMAKE_CURRENT_SOURCE_DIR}/../linker.ld")
 add_link_options("${CMAKE_CURRENT_SOURCE_DIR}/../musl/lib/libc.a")
 add_link_options("${CMAKE_CURRENT_SOURCE_DIR}/../musl/lib/crt1.o")
 add_link_options("-lgcc")
-set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} -I ${CMAKE_CURRENT_SOURCE_DIR}/../musl/include -I ${CMAKE_CURRENT_SOURCE_DIR}/../musl/obj/include -I ${CMAKE_CURRENT_SOURCE_DIR}/../musl/arch/i386/ -I ${CMAKE_CURRENT_SOURCE_DIR}/../musl/arch/generic/")
+
+set (USERSPACE_FLAGS "-I ${CMAKE_CURRENT_SOURCE_DIR}/../musl/include -I ${CMAKE_CURRENT_SOURCE_DIR}/../musl/obj/include -I ${CMAKE_CURRENT_SOURCE_DIR}/../musl/arch/i386/ -I ${CMAKE_CURRENT_SOURCE_DIR}/../musl/arch/generic/")
+set (CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${USERSPACE_FLAGS}")
+set (CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${USERSPACE_FLAGS}")
